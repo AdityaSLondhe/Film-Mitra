3b070af3184c374ed9b6a28498453dfc852d423f
Aditya Londhe <114740433+AdityaSLondhe@users.noreply.github.com>
2023-05-07 17:14:51 +0530

    front page changes

--- index.js
+++ index.js
@@ -14,5 +14,24 @@ cardContainers.forEach((item,i)=>{
         item.scrollLeft -=containerWidth-200;
     });
 });
-
+// let movies=[
+//     "images/poster 1.png",
+//     "images/poster 2.png",
+//     "images/poster 3.png",
+//     "images/poster 4.png",
+//     "images/poster 5.png",
+//     "images/poster 6.png",
+//     "images/poster 7.png",
+//     "images/poster 8.png",
+//     "images/poster 9.png",
+//     "images/poster 10.png",
+//     "images/poster 11.png",
+//     "images/poster 12.png"
+// ];
+// var SearchImg =movies[Math.floor(Math.random()*12)];
+// var el=document.querySelector(".Search-Bar").style;
+// setInterval(()=>{
+//     var SearchImg =movies[Math.floor(Math.random()*12)];
+//     document.querySelector(".Search-Bar").style.backgroundImage="uel("+SearchImg+")";
+// },1000);
     
